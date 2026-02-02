bf0a2aa7971d2a2e31ce69869498096d8626b99c
Trương Minh Phương <truongminhphuong4@gmail.com>
2026-02-02 09:17:00 +0700

    fix hehe

--- code/networks/net_factory.py
+++ code/networks/net_factory.py
@@ -1,7 +1,7 @@
 from networks.efficientunet import Effi_UNet
 from networks.enet import ENet
 from networks.pnet import PNet2D
-from networks.unet import UNet, UNet_DS, UNet_URPC, UNet_CCT, UNet_URPC_FFC
+from networks.unet import UNet, UNet_DS, UNet_URPC, UNet_CCT
 import argparse
 from networks.vision_transformer import SwinUnet as ViT_seg
 from networks.config import get_config
